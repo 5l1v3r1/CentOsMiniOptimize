61e596e5e92bd32f63313833351ad631d2a76c1b
ahmeth4n <ahmethangfx@gmail.com>
2018-08-05 04:31:53 -0700

    CentOS - Apache mini optimize tool

---
+++ README.txt
@@ -0,0 +1,5 @@
+Toolun hicbir numarasi yok. Internette bulunan servisleri tek tek kapatacaginiza
+direk buradan kapatirsiniz diye otomatiklestirdim. Kalkip avelin teki sen yazmamisin
+falan diyip beni dellendirmesin diye not duseyim dedim.
+
+Hadi iyi optimizeler , bol gunesler.
---
+++ centos_optimize.sh
@@ -0,0 +1,50 @@
+# CentOS Mini Optimization Tool ~ ahmeth4n.org
+RED='\033[0;31m'
+NC='\033[0m'
+printf "CentOS mini optimization tool . coded by ${RED}BlackDewiL${NC}.\n"
+printf "ahmeth4n.org ~ ${RED}twitter.com/inject0r16${NC}.\n"
+service cups stop
+chkconfig cups off
+service xfs stop
+chkconfig xfs off
+service atd stop
+chkconfig atd off
+service nfslock stop
+chkconfig nfslock off
+service canna stop
+chkconfig canna off
+service FreeWnn stop
+chkconfig FreeWnn off
+service cups-config-daemon stop
+chkconfig cups-config-daemon off
+service iiim stop
+chkconfig iiim off
+service mDNSResponder stop
+chkconfig mDNSResponder off
+service nifd stop
+chkconfig nifd off
+service rpcidmapd stop
+chkconfig rpcidmapd off
+service bluetooth stop
+chkconfig bluetooth off
+service anacron stop
+chkconfig anacron off
+service gpm stop
+chkconfig gpm off
+service saslauthd stop
+chkconfig saslauthd off
+service avahi-daemon stop
+chkconfig avahi-daemon off
+service avahi-dnsconfd stop
+chkconfig avahi-dnsconfd off
+service hidd stop
+chkconfig hidd off
+service pcscd stop
+chkconfig pcscd off
+service sbadm stop
+chkconfig sbadm off
+service webmin stop
+chkconfig webmin off
+service ossec stop
+chkconfig ossec off
+printf "${RED}optimization finished B-){NC}\n"
